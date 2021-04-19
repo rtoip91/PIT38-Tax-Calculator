2ada5dfa36f42e5843ce95f6bf3661c1c47c44dd
rtoip91 <martoip123@gmail.com>
2021-04-19 16:46:55 +0200

    Added delete  from previous import on start

--- EtoroExcelReader/ExcelDataExtractor.cs
+++ EtoroExcelReader/ExcelDataExtractor.cs
@@ -10,6 +10,7 @@ using Database.Entities;
 using EtoroExcelReader.Dto;
 using ExcelReader.Interfaces;
 using ExcelReader.MappingProfiles;
+using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
 using OfficeOpenXml.Export.ToDataTable;
 
@@ -84,6 +85,10 @@ namespace ExcelReader
                     transactionReportEntities.Add(transactionReportEntity);
                 }
 
+                await context.Database.BeginTransactionAsync();
+                await context.Database.ExecuteSqlRawAsync("Delete from TransactionReports");
+                await context.Database.ExecuteSqlRawAsync("Delete from ClosedPositions");
+              
                 await context.AddRangeAsync(closedPositionEntities);
                 await context.AddRangeAsync(transactionReportEntities);
                 
