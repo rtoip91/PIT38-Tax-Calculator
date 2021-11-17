dc68a08e9dc003a20affec2bc4e683c9f3308c99
rtoip91 <martoip123@gmail.com>
2021-11-17 11:47:16 +0100

    Clean data refactor

--- TaxEtoro/BussinessLogic/DataCleaner.cs
+++ TaxEtoro/BussinessLogic/DataCleaner.cs
@@ -9,21 +9,14 @@ namespace TaxEtoro.BussinessLogic
         async Task IDataCleaner.CleanData()
         {
             using (var context = new ApplicationDbContext())
-            {               
-                var closed = context.ClosedPositions;
-                var transactionReports = context.TransactionReports;
-                var cfdCalculations = context.CfdCalculations;
-                var cryptoCalculations = context.CryptoCalculations;
-                var stockCalculations = context.StockCalculations;
+            {
+                context.RemoveRange(context.ClosedPositions);
+                context.RemoveRange(context.TransactionReports);
+                context.RemoveRange(context.CfdCalculations);
+                context.RemoveRange(context.CryptoCalculations);
+                context.RemoveRange(context.StockCalculations);
 
-                context.RemoveRange(closed);
-                context.RemoveRange(transactionReports);
-                context.RemoveRange(cfdCalculations);
-                context.RemoveRange(cryptoCalculations);
-                context.RemoveRange(stockCalculations);
-
-                await context.SaveChangesAsync();
-                
+                await context.SaveChangesAsync();                
             }
         }
     }
