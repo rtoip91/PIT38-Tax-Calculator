c1c7b955d70315daba3530c01337b4ca43574732
Piotr Maćkowiak <martoip123@gmail.com>
2022-01-06 14:14:32 +0100

    Test file changed

--- Database/DataAccess/StockEntityDataAccess.cs
+++ Database/DataAccess/StockEntityDataAccess.cs
@@ -1,9 +1,6 @@
 ﻿using Database.DataAccess.Interfaces;
 using Database.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Database.DataAccess
