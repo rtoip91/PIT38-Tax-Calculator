3725394a1be6c41d23069ee38cce944e104282c4
Piotr Maćkowiak <martoip123@gmail.com>
2022-02-26 17:54:28 +0100

    New Entities

---
+++ Database/Entities/PurchasedCryptoEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Database.Entities
+{
+    [Table("PurchasedCryptoCalculations")]
+    public class PurchasedCryptoEntity
+    {
+        [Key] public int Id { get; set; }
+        public long PositionId { get; set; }
+        public string Name { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal Units { get; set; }
+        public decimal ValuePerUnit { get; set; }
+        public decimal TotalValue { get; set; }
+        public string CurrencySymbol { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public decimal TotalExchangedValue { get; set; }
+    }
+}
